e6ef1e2b2f6ebe8094610240fbc80cf4d87ba34b
Samiran Das <samirandascr@gmail.com>
2024-04-07 11:46:03 +1000

    Login page development

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ app = Flask(__name__)
 
 # Initialize DynamoDB client
 dynamodb = boto3.resource('dynamodb',
-                  aws_access_key_id="ASIAXYKJVXP7X7P4VGAK",
-                  aws_secret_access_key="kBu4lGMCX2IytpPjySIHGPw9696WZFiGfLoyifbi",
-                  aws_session_token=r"IQoJb3JpZ2luX2VjEJX//////////wEaCXVzLXdlc3QtMiJHMEUCIBkzp/0gKoFIeePh5a7c7rANQwc6ELWG1lSdMijvgcz/AiEA8dpYZDxmyccvYlRVuYHnF0pVUv3o3plbFJfzgjZ5pWEqvwIIvv//////////ARAAGgw1MzMyNjczMzIwOTUiDAHLcdqM6tlafsUiRyqTAmcA8MklDTKVmhl6PgJfAcp80cfNHN0c5oKAkiKAw925NjtvsbnozbjX6SmblOrhZD25ygw/b32ucwWyF6NtgJKnmN01DVgN35DYa5n/9GGBCCwd0Va6re6p+1DqGKZrL1FcEwneXRQ48Qy0qep+6BDCfMGKIqArDVWyCEVYLROK+eaG3FpHKSoH/wYfXJbW8uENMTmLBNQxHplUdoZBn9NeWy5xWXWm6HISI9rbNdpoKsQkGq6Lhz+qS+xkFzw2JlyMjQyDTl0ZlV6vb1iGljN2XgkyHKxDTLHaUq9+DHucO5yP5NDCdi3FFxmKXQavkf2J5ei/RVIBwBvXkQK2IO6QFODCtNc+yhrVj/3WftsGBuxbMICTxbAGOp0BOWKGWHMhnA+MwNy+8teiWJrFJgpDpcXySEpfk4qp71Znox2HAT0KN9AQEhJ17vxeV687uG2W3Bjjbqb/ErS5NErHo4Tn7YbvTZfDD+tRMuPQRC/1NBynSJCgp4/uMikjT3lwKYnt6E7QNvOiReb/ud7zSJkAuNSjCp5EAA2XpYcaOhYw/y2rKkmlzOBT9+xrtokUoB/buqhGz+9SRA==",
+                  aws_access_key_id="ASIAXYKJVXP7XAR4DYDX",
+                  aws_secret_access_key="D3wmjSmzHVhVY466/0tZes4x6L4mhXu02HJzER/3",
+                  aws_session_token=r"IQoJb3JpZ2luX2VjEKL//////////wEaCXVzLXdlc3QtMiJGMEQCIFbHKKQhATZ5WHR85nn9a3OUPROYeijhYEALU4RQuriwAiA3pza0uJBZQU/teJCVu32TKbZJql3I5P+uGm0FdSc09yq/AgjL//////////8BEAAaDDUzMzI2NzMzMjA5NSIMX340Yx9GlPeWSa7EKpMC1WCOmGInh+f+qIcXXRZRO70yWq7EnuYf/DqaDeVsti47XMsa5/llYKbEjGrqqfMlEmZF7uIcov3lblOS8z7SfwExTIlDT81pvIsJwBiTg10jvugMoVTdLvoxeGh/CrSpM0sOlxEvjKth4akBeNAT3TkGBz8seKAbDC3nKgGNfmgOtn36T75izh3QyuaXCHY12q/f258iNjN28VjWcigS1J0WkcEmBqeE4mIGxE0EVeQ1/vE2QlK7OoDXyO5i+HoRCjVfx6awEHu82m2EJ5eieMMCxiMaLeTgE28OvHSxuGTo0fmZEMMZAm/x91m3dSnmyrvASXwGk2sywvYOj/80mY/lM5w1w2kxzzIqfOwfviWudZww/+zHsAY6ngEOuuJEjGIjeIzoHbVYix2pCXbO+tVFSVaHqPuU8pOQvy3YIxdtVV14s3+3pcwxO+NsF7LWo20e1lwgOVpfoJJS7xTVdze7O4BdpN1XZSMhL6xV1o8kAndx7DsYdF92+W/Yfsybp8+kcgSt30x7+Ea7h68LES2IwF7HO8hRd20OfSmF9kvC2Iw7CEBiAL5+bl6Gr+5sibO9eRM01nzDsA==",
                   region_name="us-east-1")
 
 # DynamoDB table name
